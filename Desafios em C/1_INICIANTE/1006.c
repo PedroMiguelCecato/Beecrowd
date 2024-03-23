acf7f87ecdf3733349fb4836f55eb34c78c7f27c
Pedro Miguel Cecato Valões <pedro.cecato@academico.ufpb.br>
2024-03-23 16:32:28 -0300

    update

--- Desafios em C/1_INICIANTE/1006.c
+++ Desafios em C/1_INICIANTE/1006.c
@@ -4,8 +4,9 @@ int main() {
     double nota1, nota2, nota3, media;
     
     scanf("%lf %lf %lf", &nota1, &nota2, &nota3);
-    media = ((nota1*2) + (nota2*3) + (nota3*5))/10;
     
+    media = ((nota1*2) + (nota2*3) + (nota3*5))/10;
     printf("MEDIA = %.1lf\n", media);
+
     return 0;
 }
--- Desafios em C/1_INICIANTE/1011.c
+++ Desafios em C/1_INICIANTE/1011.c
@@ -4,8 +4,9 @@ int main() {
     double volume, raio;
     
     scanf("%lf", &raio);
+
     volume = (4.0/3)*3.14159*raio*raio*raio;
-    
     printf("VOLUME = %.3lf\n", volume);
+    
     return 0;
 }
